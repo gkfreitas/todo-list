75eb3a8764d9dbc856485c5d505fe00b6746e6eb
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-07 18:37:23 -0300

    Requsito 8 finalizado

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const buttonAdd = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const inputTask = document.getElementById('texto-tarefa');
-const listChilds = list.children;
+const listChilds = list.childNodes;
+// Adicionar Tarefa
 function addTask() {
   const createLi = document.createElement('li');
   const inputValue = inputTask.value;
@@ -11,11 +12,16 @@ function addTask() {
   console.log(listChilds.length);
 
   // Add color
+  function selectTask(task) {
+    const taskClick = task.target;
+    for (let i = 0; i < listChilds.length; i += 1) {
+      listChilds[i].style = 'white';
+    }
+    taskClick.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
   for (let i = 0; i < listChilds.length; i += 1) {
-    listChilds[i].addEventListener('click', (li) => {
-      const event = li.target;
-      event.style.backgroundColor = 'gray';
-    });
+    listChilds[i].addEventListener('click', selectTask);
   }
 }
+
 buttonAdd.addEventListener('click', addTask);
